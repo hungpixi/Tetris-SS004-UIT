33139eef21e505b43036335ece8fa8438121e958
Phạm Phú Nguyễn Hưng <ppnh10092002@gmail.com>
2026-08-28 09:19:14 +0700

    feat(audio): Tich hop am thanh Beep va co che luu HighScore buoc 6

--- audio_highscore.cpp
+++ audio_highscore.cpp
@@ -3,3 +3,4 @@
 // Step 3: Am thanh Beep tan so cao va doc/ghi file highscore.txt buoc 3
 // Step 4: Am thanh Beep tan so cao va doc/ghi file highscore.txt buoc 4
 // Step 5: Am thanh Beep tan so cao va doc/ghi file highscore.txt buoc 5
+// Step 6: Am thanh Beep tan so cao va doc/ghi file highscore.txt buoc 6
