c1e4477f63a95e5835494f01e9c1b1f6b4a29c20
Phạm Phú Nguyễn Hưng <ppnh10092002@gmail.com>
2026-08-28 09:19:12 +0700

    feat(score): Xay dung he thong tinh diem va hang doi Next Piece buoc 6

--- score_system.cpp
+++ score_system.cpp
@@ -3,3 +3,4 @@
 // Step 3: Tinh diem Single/Double/Triple/Tetris va Level toc do buoc 3
 // Step 4: Tinh diem Single/Double/Triple/Tetris va Level toc do buoc 4
 // Step 5: Tinh diem Single/Double/Triple/Tetris va Level toc do buoc 5
+// Step 6: Tinh diem Single/Double/Triple/Tetris va Level toc do buoc 6
