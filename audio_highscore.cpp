66d7c6befd395b6e4965ceabf8ecee6c42d371ad
Vo Thi Kim Em <23520005@gm.uit.edu.vn>
2026-08-28 09:13:46 +0700

    feat(audio): SV5 tich hop am thanh Beep va luu file HighScore buoc 5

--- audio_highscore.cpp
+++ audio_highscore.cpp
@@ -2,3 +2,4 @@
 // Step 2: Beep audio frequency, highscore.txt file I/O step 2
 // Step 3: Beep audio frequency, highscore.txt file I/O step 3
 // Step 4: Beep audio frequency, highscore.txt file I/O step 4
+// Step 5: Beep audio frequency, highscore.txt file I/O step 5
